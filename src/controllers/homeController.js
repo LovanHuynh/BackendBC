f63304c9cb6fc4bf8438a24ed29f72bcc4746873
Endada <huynhsth@gmail.com>
2024-05-24 01:26:46 +0700

    dat lich phong kham

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index';
+import CRUDservice from '../services/CRUDservice';
 
 let getHomePage = async (req, res) => {
     try {
@@ -11,6 +12,31 @@ let getHomePage = async (req, res) => {
     }
 
 }
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+let postCRUD = async (req, res) => {
+    let message = await CRUDservice.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud form lable');
+}
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDservice.getAllUser();
+    console.log('------------------------');
+    console.log(data);
+    console.log('------------------------');
+
+    return res.render('displayCRUD.ejs', {
+        dataTable: data,
+    });
+}
+
+
+
+
 module.exports = {
     getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
 }
--- src/models/user.js
+++ src/models/user.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
     phonenumber: DataTypes.STRING,
     gender: DataTypes.BOOLEAN,
     image: DataTypes.STRING,
-    rodeId: DataTypes.STRING,
+    roleId: DataTypes.STRING,
     positionId: DataTypes.STRING,
   }, {
     sequelize,
