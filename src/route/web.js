d12a9def5e3efefc58102a43be01af45411264d6
Endada <huynhsth@gmail.com>
2024-07-31 13:10:08 +0700

    demo

--- src/route/web.js
+++ src/route/web.js
@@ -9,17 +9,17 @@ let router = express.Router();
 
 let initWebRouters = (app) => {
 
-    router.get('/', homeController.getHomePage);
+    // router.get('/', homeController.getHomePage);
 
-    router.get('/crud', homeController.getCRUD);// tao nguoi dung 
-    router.post('/post-crud', homeController.postCRUD);
+    // router.get('/crud', homeController.getCRUD);// tao nguoi dung 
+    // router.post('/post-crud', homeController.postCRUD);
 
-    router.get('/get-crud', homeController.displayGetCRUD);// hien thi nguoi dung len trang web
+    // router.get('/get-crud', homeController.displayGetCRUD);// hien thi nguoi dung len trang web
 
-    router.get('/edit-crud', homeController.getEditCRUD);//lay thong tin sau do gui vao form sua
-    router.post('/put-crud', homeController.putCRUD);
+    // router.get('/edit-crud', homeController.getEditCRUD);//lay thong tin sau do gui vao form sua
+    // router.post('/put-crud', homeController.putCRUD);
 
-    router.get('/delete-crud', homeController.deleteCRUD);// xoa nguoi dung
+    // router.get('/delete-crud', homeController.deleteCRUD);// xoa nguoi dung
 
     //bat dau viet api
     router.post('/api/login', userController.handleLogin);
